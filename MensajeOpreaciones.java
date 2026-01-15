239e0be5044c4537ef92a132829285ed6b51c165
Mauro-2107 <maullopic@alu.edu.gva.es>
2026-01-15 12:25:13 +0100

    Crear MensajeOperaciones.java

---
+++ MensajeOpreaciones.java
@@ -0,0 +1,8 @@
+public class MensajeOperaciones{
+  public void mostrarMensajeInicio(){
+    System.out.println("Bienvenido al programa");
+  }
+  public void mostrarMensajeFin(){
+    System.out.println("¡Gracias por usar el programa de operaciones!");
+  }
+}
